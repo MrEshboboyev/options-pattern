39d97d1633bfce5f4d0cfc2756f5d46ae3b9f349
MrEshboboyev <mreshboboyev@gmail.com>
2025-03-26 11:32:32 +0500

    Add tests for Custom Fluent Validation

---
+++ tests/Options.Pattern.Tests/ValidationTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.TestHelper;
+using Options.Pattern.Api.Settings;
+using Options.Pattern.Api.Validators;
+
+namespace Options.Pattern.Tests;
+
+public class ValidationTests
+{
+    // Uses helper methods from FluentValidation.TestHelper
+    [Fact]
+    public void GitHubSettings_WithMissingAccessToken_ShouldHaveValidationError()
+    {
+        // Arrange
+        var validator = new GitHubSettingsValidator();
+        var settings = new GitHubSettings 
+        { 
+            BaseUrl = "https://base_url.com",
+            AccessToken = "access-token",
+            RepositoryName = "test-repo" 
+        };
+
+        // Act
+        TestValidationResult<GitHubSettings>? result = validator.TestValidate(settings);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
